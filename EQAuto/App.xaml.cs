67eb0d417583e6a5029f51a858a44d67faaeb05a
Glorifundel <Glorifundel@gmail.com>
2017-10-18 23:27:47 -0400

    Add project files.

---
+++ EQAuto/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace EQAuto
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="log"></param>
+        /// <param name="prefix"></param>
+        /// <param name="suffix"></param>
+        public static void Log(string log, string prefix = "====", string suffix = "====")
+        {
+            System.Diagnostics.Debug.WriteLine(prefix + log + suffix);
+        }
+    }
+}
---
+++ EQAuto/Storage.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EQAuto
+{
+
+
+    class Storage
+    {
+
+        struct Constants
+        {
+            public const string CONFIGURATION_FILE = "configuration.json";
+        }
+
+        private static string dataPath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "DarknessDescending\\EQAuto");
+        private static string configurationData = System.IO.Path.Combine(dataPath, Constants.CONFIGURATION_FILE);
+
+        public static bool Exists(string path)
+        {
+            if (File.Exists(path))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static string Read(string path)
+        {
+            if (File.Exists(path))
+            {
+                return File.ReadAllText(path);
+            }
+            return null;
+        }
+
+        public static bool Write(string path, string data)
+        {
+            try
+            {
+                if (!File.Exists(path))
+                {
+                    Directory.CreateDirectory(folderPath);
+                }
+                File.WriteAllText(path, data);
+            }
+            catch (Exception e)
+            {
+                App.Log($"Storage.Write({path}, {data}) error: {e.Message}");
+            }
+            return true;
+        }
+    }
+}
